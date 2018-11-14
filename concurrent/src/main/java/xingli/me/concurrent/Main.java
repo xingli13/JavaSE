a6509b04210717af780471eaaad008c3cbaca932
xingli13 <lxxdev@qq.com>
2018-11-14 16:15:57 +0800

    init

---
+++ concurrent/src/main/java/xingli/me/concurrent/Main.java
@@ -0,0 +1,11 @@
+package xingli.me.concurrent;
+
+/**
+ * @author xingli13
+ * @date 2018/11/14
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("concurrent");
+	}
+}
